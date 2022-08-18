17691a27a22e73208e40fde7167f7debf2e8d00d
Rodrigo Bonifácio <rbonifacio123@gmail.com>
2022-08-18 16:58:01 -0300

    monadic version of error handling

--- src/main/scala/br/unb/cic/Expression.scala
+++ src/main/scala/br/unb/cic/Expression.scala
@@ -11,25 +11,22 @@ abstract class M[A, B]
 case class Value[A,B](v: A) extends M[A, B]
 case class Error[A,B](v: B) extends M[A, B]
 
+object M {
+  def pure[A, B](v: A) : M[A,B] = Value(v)
+
+  def bind[A,B, C](m: M[A, B], f : A => M[C,B]): M[C,B] = m match {
+    case Value(v) => f(v)
+    case Error(b) => Error(b)
+  }
+}
+
+import M._
 /** Variation one of eval: Error handling */
 object Expression {
   def eval(exp: Expression): M[Integer, String] = exp match {
     case Const(v)  => Value(v)
-    case Add(l, r) => {
-      (eval(l), eval(r)) match {
-        case (Value(v1), Value(v2)) => Value(v1 + v2)
-        case (Error(s), _) => Error(s)
-        case (_, (Error(s))) => Error(s)
-      }
-    }
-    case Div(l, r) => {
-      (eval(l), eval(r)) match {
-        case (Value(v1), Value(0))  => Error("division by zero")
-        case (Value(v1), Value(v2)) => Value(v1 / v2)
-        case (Error(s), _) => Error(s)
-        case (_, Error(s)) => Error(s)
-      }
-    }
+    case Add(l, r) => bind(eval(l), (x: Integer) => bind(eval(r), (y: Integer) => pure(x + y)))
+    case Div(l, r) => bind(eval(l), (x: Integer) => bind(eval(r), (y: Integer) => if(y == 0) Error("Division by zero") else pure(x + y)))
   }
 }
 
