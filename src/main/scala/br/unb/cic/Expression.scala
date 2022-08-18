58ec88c7b9d94bc5fd952c9d8f87816fe7232416
Rodrigo Bonifácio <rbonifacio123@gmail.com>
2022-08-18 20:23:49 -0300

    Adding test cases

--- src/main/scala/br/unb/cic/Expression.scala
+++ src/main/scala/br/unb/cic/Expression.scala
@@ -3,30 +3,32 @@ package br.unb.cic
 sealed trait Expression
 
 case class Const(val value: Integer) extends Expression
-case class Add(val left: Expression, val right: Expression)
-case class Div(val left: Expression, val right: Expression)
+case class Add(val left: Expression, val right: Expression) extends Expression
+case class Div(val left: Expression, val right: Expression) extends Expression
 
-abstract class M[A, B]
-
-case class Value[A,B](v: A) extends M[A, B]
-case class Error[A,B](v: B) extends M[A, B]
+sealed trait M[A]
 
 object M {
-  def pure[A, B](v: A) : M[A,B] = Value(v)
+  type Message = String
+  def pure[A](v: A) : M[A] = Value(v)
 
-  def bind[A,B, C](m: M[A, B], f : A => M[C,B]): M[C,B] = m match {
+  def bind[A,B](m: M[A], f : A => M[B]): M[B] = m match {
     case Value(v) => f(v)
     case Error(b) => Error(b)
   }
 }
 
 import M._
+
+case class Value[A](v: A) extends M[A]
+case class Error[A](v: Message) extends M[A]
+
 /** Variation one of eval: Error handling */
 object Expression {
-  def eval(exp: Expression): M[Integer, String] = exp match {
+  def eval(exp: Expression): M[Integer] = exp match {
     case Const(v)  => Value(v)
     case Add(l, r) => bind(eval(l), (x: Integer) => bind(eval(r), (y: Integer) => pure(x + y)))
-    case Div(l, r) => bind(eval(l), (x: Integer) => bind(eval(r), (y: Integer) => if(y == 0) Error("Division by zero") else pure(x + y)))
+    case Div(l, r) => bind(eval(l), (x: Integer) => bind(eval(r), (y: Integer) => if(y == 0) Error("Division by zero") else pure(x / y)))
   }
 }
 
