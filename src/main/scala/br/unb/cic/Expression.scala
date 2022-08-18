3132d860f8aaa36448eb0c3b5f3dddcec85d2742
Rodrigo Bonifácio <rbonifacio123@gmail.com>
2022-08-18 16:17:05 -0300

    Error handling

--- src/main/scala/br/unb/cic/Expression.scala
+++ src/main/scala/br/unb/cic/Expression.scala
@@ -6,12 +6,30 @@ case class Const(val value: Integer) extends Expression
 case class Add(val left: Expression, val right: Expression)
 case class Div(val left: Expression, val right: Expression)
 
-/** Variation zero of eval */
+abstract class M[A, B]
+
+case class Value[A,B](v: A) extends M[A, B]
+case class Error[A,B](v: B) extends M[A, B]
+
+/** Variation one of eval: Error handling */
 object Expression {
-  def eval(exp: Expression): Integer = exp match {
-    case Const(v)  => v
-    case Add(l, r) => eval(l) + eval(r)
-    case Div(l, r) => eval(l) / eval(r)
+  def eval(exp: Expression): M[Integer, String] = exp match {
+    case Const(v)  => Value(v)
+    case Add(l, r) => {
+      (eval(l), eval(r)) match {
+        case (Value(v1), Value(v2)) => Value(v1 + v2)
+        case (Error(s), _) => Error(s)
+        case (_, (Error(s))) => Error(s)
+      }
+    }
+    case Div(l, r) => {
+      (eval(l), eval(r)) match {
+        case (Value(v1), Value(0))  => Error("division by zero")
+        case (Value(v1), Value(v2)) => Value(v1 / v2)
+        case (Error(s), _) => Error(s)
+        case (_, Error(s)) => Error(s)
+      }
+    }
   }
 }
 
